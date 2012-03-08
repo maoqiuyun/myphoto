d8d329d5629f7bb44b84ed627d6b09f53f5f7b99
maoqiuyun <maoqiuyun@ekohe.com>
2012-03-08 11:36:34 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myphoto::Application.config.secret_token = '4ea995bc04d1a1b4a4106cfd39d7085d7deaf88dd07a3ed7a51f729f651f1b64a39efcd4ef506955eab6655fb2e8384567e7dd684637fb3f22bfeeb3af090bee'
