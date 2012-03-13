482c99545e5cfedda9b210ac1e6ab3ce6c9448d1
maoqiuyun <maoqiuyun@ekohe.com>
2012-03-13 18:43:16 +0800

    gitignore

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'carrierwave/processing/mini_magick'
 
 class ImageUploader < CarrierWave::Uploader::Base
 
@@ -29,9 +30,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #    process :scale => [50, 50]
-  #  end
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
